b9ab734f6ad2138311fc7c483232dae410488a52
Nidhi <nidhi1997.jsr@gmail.com>
2024-10-08 20:42:47 +0530

    added controller and services

--- src/main/java/com/dev/productcatalogservice/EcommerceWebsiteApplication.java
+++ src/main/java/com/dev/productcatalogservice/EcommerceWebsiteApplication.java
@@ -2,6 +2,8 @@ package com.dev.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class EcommerceWebsiteApplication {
@@ -9,5 +11,7 @@ public class EcommerceWebsiteApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceWebsiteApplication.class, args);
     }
+
+
 
 }
